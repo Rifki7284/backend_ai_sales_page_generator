1f5f6fd6e946c3dd6270ce9a20d243786247b99e
Rifki7284 <rifkiahmadrivera07@gmail.com>
2026-04-30 09:32:06 +0700

    feat:vercel config

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\SalesPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,23 +8,4 @@ Route::get('/', function () {
 
 Route::get('/test', function () {
     return response()->json('hi');
-});
-Route::prefix('api/v2')->group(function () {
-    Route::prefix('auth')->group(function () {
-        Route::post('/register', [AuthController::class, 'register']);
-        Route::post('/login', [AuthController::class, 'login']);
-    });
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/user', fn (Request $request) => $request->user());
-        Route::post('/auth/logout', [AuthController::class, 'logout']);
-
-        // Sales Pages Resource
-        Route::prefix('pages')->group(function () {
-            Route::get('/', [SalesPageController::class, 'index']);
-            Route::post('/generate', [SalesPageController::class, 'generate']);
-            Route::get('/{id}', [SalesPageController::class, 'show']);
-            Route::put('/{id}', [SalesPageController::class, 'update']);
-            Route::delete('/{id}', [SalesPageController::class, 'destroy']);
-        });
-    });
 });
